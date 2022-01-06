1d12330c8b07d478102a277d926e2848312b3a09
Saatwik Mehta <saatwikmehta@gmail.com>
2022-01-07 01:54:43 +0530

    get the list of dictionaries that contains the group name(key) and the name of the group members as a list(value)

---
+++ group_data.py
@@ -0,0 +1,23 @@
+"""
+This module will give you the
+list of members of groups in your account
+"""
+from boto3 import client
+
+
+def get_group_members():
+    """This function will return the List of dictionaries
+     that contains the group name and members inside
+        a particular aws account.
+    :return: List of dictionaries containing the group name and members
+    """
+    iam_client = client('iam')
+    groups = iam_client.list_groups()
+    group_names = [group['GroupName'] for group in groups['Groups']]
+    return list({grp: [users['UserName']
+                       for users in iam_client.get_group(GroupName=grp)['Users']]}
+                for grp in group_names)
+
+
+if __name__ == '__main__':
+    print(get_group_members())
